3b98baa5f470f6e6c5c0ff5f0c04e78f95083c08
mayank <mayankbatra1@icloud.com>
2022-06-08 16:50:40 +0530

    Initial Commit

---
+++ Shared/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Shared
+//
+//  Created by Mayank Batra on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
